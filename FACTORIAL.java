8a9eb9408815ab0cf881ae8366a55f6457d393d8
11041990 <vela3719diaz@gmail.com>
2025-04-06 16:34:58 -0600

    factorial de un numero actividad 2 diaz vela mario de jesus

---
+++ FACTORIAL.java
@@ -0,0 +1,16 @@
+
+package factorial;
+
+public class FACTORIAL {
+
+    public static void main(String[] args) {
+        int numero = 5;
+        int factorial = 1;
+        for (int i = 1; i <= numero; i++) {
+            factorial *= i;
+            }
+        System.out.println("El factorial de " + numero + " es: " + factorial);
+        }
+    }
+    
+ 
